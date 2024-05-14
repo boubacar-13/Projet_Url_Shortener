15d31880d8d97415dd023952348587b74ba8a1a5
Boubacar <timera.boubacar@gmail.com>
2024-05-14 15:54:56 +0200

    Create and call helpers methods

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -17,3 +17,26 @@ type response struct {
 	RateRemaining	int			`json:"rate_limit"`
 	RateLimitReset	time.Duration	`json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Cannot parse JSON"})
+	}
+
+	//implement rate limiting
+
+	//check if the input is an actual URL
+	if !govalidator.IsURL(body.URL){
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invalid URL"})
+	}
+
+	//check for domain error
+		if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error":"You can't access :)"})
+	}
+
+	//enforce https, SSL
+	body.URL = helpers.EnforceHTTP(body.URL)
+}

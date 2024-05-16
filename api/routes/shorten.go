409888a82562039bf731380a033b3a013cc74cbd
Boubacar <timera.boubacar@gmail.com>
2024-05-16 16:51:19 +0200

    Managing response on post

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -91,5 +91,23 @@ func ShortenURL(c *fiber.Ctx) error {
 		})
 	}
 
+	resp := response{
+		URL: 			body.URL,
+		CustomShort:		"",
+		Expiry:			body.Expiry,
+		RateRemaining: 	10,
+		RateLimitReset:	30
+	}
+
 	r2.Decr(database.Ctx, c.IP())
+
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.RateRemaining, _ = strconv.Atoi(val)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+	resp.RateLimitReset, _ = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
 }

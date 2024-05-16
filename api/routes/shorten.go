070848cc856247fa9aeaf60b4c2917fd223fb7ee
Boubacar <timera.boubacar@gmail.com>
2024-05-16 16:22:50 +0200

    Implement rate limiting

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,9 @@ package routes
 
 import(
 	"time"
+	"os"
+	"github.com/boubacar-13/Projet_Url_Shortener/datbase"
+
 )
 
 type request struct {
@@ -26,6 +29,24 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	//implement rate limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil{
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+
+		if valInt <= 0{
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":"Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	//check if the input is an actual URL
 	if !govalidator.IsURL(body.URL){
@@ -39,4 +60,6 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	//enforce https, SSL
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r2.Decr(database.Ctx, c.IP())
 }
